be38e0716c853edf3b3ef59f9abc942fd0698397
ShaoLei1211 <Ashao19@ndhsb.org>
2019-03-20 09:06:59 -0700

    Update Main.java

--- Cash Register/Main.java
+++ Cash Register/Main.java
@@ -2,12 +2,19 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-	ArrayList<Product> product = new ArrayList<>();
-	product.add(new Cantared_Apple());
+	ArrayList<Product> item = new ArrayList<>();
+	item.add(new Cantared_Apple());
+	item.add(new Yellow_Squash());
 
-        System.out.println(product.size());
-        for (Product x :product){//every object is referred as x, so it can check
+        System.out.println("Here are "+item.size()+" items created in total.\n");
+
+        for (Product x :item){//every object is referred as x, so it can check
             if (x instanceof Cantared_Apple){
+                x.showstats();
+            }
+        }
+        for (Product x :item){//every object is referred as x, so it can check
+            if (x instanceof Yellow_Squash){
                 x.showstats();
             }
         }
