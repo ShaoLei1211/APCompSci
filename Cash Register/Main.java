d0604fcd00d32325c8812ba1bca22e0992e81e84
ShaoLei1211 <Ashao19@ndhsb.org>
2019-03-20 08:42:42 -0700

    Update Main.java

--- Cash Register/Main.java
+++ Cash Register/Main.java
@@ -3,6 +3,14 @@ public class Main {
 
     public static void main(String[] args) {
 	ArrayList<Product> product = new ArrayList<>();
+	product.add(new Cantared_Apple());
+
+        System.out.println(product.size());
+        for (Product x :product){//every object is referred as x, so it can check
+            if (x instanceof Cantared_Apple){
+                x.showstats();
+            }
+        }
 
 
     }
